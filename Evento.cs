e403a426335be854da859a72d6bbe2ec8e796e9c
Fesposito9197 <kekkoesp8@gmail.com>
2023-03-27 14:51:38 +0200

    aggiunge milestone 1

---
+++ Evento.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class Evento
+    {
+        string titolo;
+        DateTime data;
+        int maxcapacity;
+        int reservedseats;
+        string error = "Hai inserito un dato non valido";
+
+        public Evento(string titolo, DateTime data, int maxcapacity)
+        {
+            this.titolo = titolo;
+            this.data = data;
+            this.maxcapacity = maxcapacity;
+            this.reservedseats = 0;
+        }
+
+        public int PrenotaPosti( int _posti)
+        {
+            if (data > DateTime.Now && maxcapacity > reservedseats)
+            {
+                return reservedseats += _posti;
+            }
+            else
+            {
+                throw new Exception("Posti per l'evento terminati");
+            }
+        }
+        public int DisdiciPosti(int _posti)
+        {
+            if (data > DateTime.Now &&  reservedseats > _posti)
+            {
+                return reservedseats -= _posti; 
+            }
+            else 
+            {
+                throw new Exception("Non puoi togliere piu posti di quelli che hai prenotato!!");
+                    
+            }
+        }
+        public override string ToString()
+        {
+            var dataTitolo = $"{setTitolo(titolo)} - {setMaxCapacity(maxcapacity)} - " + setData(data).ToString("dd/MM/yyyy"); 
+            return dataTitolo;
+        }
+        public string getTitolo()
+        {
+            return titolo;
+        }
+        public string setTitolo( string _titolo)
+        {
+            if (_titolo != null && _titolo != "")
+            {
+                return this.titolo = _titolo;
+            }
+            else
+            {
+                Console.WriteLine(error);
+                throw new Exception();
+            }
+        }
+            
+            
+            
+        public DateTime setData(DateTime _data)
+        {
+            var currentData = DateTime.Now;
+
+            if (_data > currentData)
+            {
+                return this.data = _data;
+            }
+            else
+            {
+                Console.WriteLine(error);
+                throw new Exception();
+            }
+            
+        }
+        public DateTime getData() 
+        {
+            return data;
+        }
+        public int getMaxcapacity() 
+        {
+            return maxcapacity;
+        }
+        public int setMaxCapacity(int _capacity)
+        {
+            if (_capacity < 0)
+            {
+                
+                Console.WriteLine(error);
+                throw new Exception();      
+            }
+            else
+            {
+                return this.maxcapacity = _capacity; 
+            }
+
+        }
+       
+        public int getReservedseats()
+        {
+            return reservedseats;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using GestoreEventi;
+
+Console.WriteLine("Inserisci un nuovo evento");
+
+Console.WriteLine();
+
+Console.WriteLine("inserisci nome evento");
+string? eventName = Console.ReadLine();
+
+Console.WriteLine("inserisci data evento (dd/MM/YYYY)");
+DateTime eventDate = Convert.ToDateTime(Console.ReadLine());
+
+Console.WriteLine("inserisci massima capacita evento");
+int maxCapacity = Convert.ToInt32(Console.ReadLine());
+
+Evento evento = new Evento(eventName, eventDate, maxCapacity);
+
+Console.WriteLine(evento);
