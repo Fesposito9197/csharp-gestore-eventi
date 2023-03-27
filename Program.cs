e41ba75270b0f0a91fffcbb668c68f9a846acd8d
Fesposito9197 <kekkoesp8@gmail.com>
2023-03-27 17:51:09 +0200

    aggiunge milestone 3 e 4

--- Program.cs
+++ Program.cs
@@ -1,9 +1,35 @@
 ﻿
 using GestoreEventi;
+Console.WriteLine("Inserisci il nome del tuo programma di eventi ");
+string? eventsName = Console.ReadLine();
+var eventProgram = new ProgrammaEventi(eventsName);
 
-//Console.WriteLine("Inserisci un nuovo evento");
+Console.WriteLine("Inserisci il numero degli eventi presenti nel programma");
+var eventsNumber = Convert.ToInt32(Console.ReadLine());
 
-//Console.WriteLine();
+for (int i = 0; i < eventsNumber; i++)
+{
+    Console.WriteLine("inserisci nome evento");
+    string? eventName = Console.ReadLine();
+
+    Console.WriteLine("inserisci data evento (dd/MM/YYYY)");
+    DateTime eventDate = Convert.ToDateTime(Console.ReadLine());
+
+    Console.WriteLine("inserisci massima capacita evento");
+    int maxCapacity = Convert.ToInt32(Console.ReadLine());
+
+    Evento evento = new Evento(eventName, eventDate, maxCapacity);
+
+    eventProgram.AddEvent(evento);
+}
+
+List<Evento> allevents = eventProgram.GetEvent();
+
+Console.WriteLine(eventProgram.PrintEvent(allevents));
+
+
+Console.WriteLine("--------------------------");
+
 
 //Console.WriteLine("inserisci nome evento");
 //string? eventName = Console.ReadLine();
@@ -13,50 +39,50 @@ using GestoreEventi;
 
 //Console.WriteLine("inserisci massima capacita evento");
 //int maxCapacity = Convert.ToInt32(Console.ReadLine());
-int maxCapacity = 100;
+//int maxCapacity = 100;
 
 //Evento evento = new Evento(eventName, eventDate, maxCapacity);
-Evento evento = new Evento("prova", Convert.ToDateTime("12/12/2024"), maxCapacity);
+//Evento evento = new Evento("prova", Convert.ToDateTime("12/12/2024"), maxCapacity);
 
-Console.WriteLine(evento);
-
-Console.WriteLine("--------------------------");
-
-Console.WriteLine("Quanti posti desideri prenotare?");
-int postiPrenotati = Convert.ToInt32(Console.ReadLine());
-evento.PrenotaPosti(postiPrenotati);
-Console.WriteLine($"Numero posti prenotati: {evento.getReservedseats()}");
-Console.WriteLine($"Numero di posti rimasti: {maxCapacity - postiPrenotati}");
+//Console.WriteLine(evento);
 
 Console.WriteLine("--------------------------");
 
-while (true)
-{
-    Console.WriteLine("Vuoi disdire dei posti prenotati? (s/n)");
-    var userChoice = Console.ReadLine();
-    if(postiPrenotati == 0 )
-    {
-        Console.WriteLine("Non hai posti prenotati");
-        break;
+//Console.WriteLine("Quanti posti desideri prenotare?");
+//int postiPrenotati = Convert.ToInt32(Console.ReadLine());
+//evento.PrenotaPosti(postiPrenotati);
+//Console.WriteLine($"Numero posti prenotati: {evento.getReservedseats()}");
+//Console.WriteLine($"Numero di posti rimasti: {maxCapacity - postiPrenotati}");
+
+//Console.WriteLine("--------------------------");
+
+//while (true)
+//{
+//    Console.WriteLine("Vuoi disdire dei posti prenotati? (s/n)");
+//    var userChoice = Console.ReadLine();
+//    if(postiPrenotati == 0 )
+//    {
+//        Console.WriteLine("Non hai posti prenotati");
+//        break;
 
 
-    }else if(userChoice is "n") 
-    {
-        Console.WriteLine("Goditi l'evento");
-        break;
+//    }else if(userChoice is "n") 
+//    {
+//        Console.WriteLine("Goditi l'evento");
+//        break;
 
 
-    }else if (userChoice is "s")
-    {
-            Console.WriteLine("Quanti posti prenotati vuoi disdire?");
-            var userDeleteSeat = Convert.ToInt32(Console.ReadLine());
-            evento.DisdiciPosti(userDeleteSeat);
-            Console.WriteLine($"I tuoi posti prenotati sono: {evento.getReservedseats()}");
-            Console.WriteLine($"I posti dell'evento ora sono: {evento.getMaxcapacity() - evento.getReservedseats()}");
+//    }else if (userChoice is "s")
+//    {
+//            Console.WriteLine("Quanti posti prenotati vuoi disdire?");
+//            var userDeleteSeat = Convert.ToInt32(Console.ReadLine());
+//            evento.DisdiciPosti(userDeleteSeat);
+//            Console.WriteLine($"I tuoi posti prenotati sono: {evento.getReservedseats()}");
+//            Console.WriteLine($"I posti dell'evento ora sono: {evento.getMaxcapacity() - evento.getReservedseats()}");
             
 
-    }
-}
+//    }
+//}
 
     
     
--- ProgrammaEventi.cs
+++ ProgrammaEventi.cs
@@ -3,11 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Environment;
 
 namespace GestoreEventi
 {
-    internal class ProgrammaEventi
+    public class ProgrammaEventi
     {
+        string? titolo;
+        List<Evento> eventi;
 
+        public ProgrammaEventi(string titolo)
+        {
+            this.titolo = titolo;
+            eventi = new List<Evento>();
+        }
+
+        public void AddEvent(Evento evento)
+        {
+            eventi.Add(evento);
+        }
+
+        public List<Evento> FindEvent(DateTime date)
+        {
+            var filterdEvents = new List<Evento>();
+
+
+            foreach(Evento evento in eventi)
+            {
+                if(evento.getData() == date)
+                {
+                    filterdEvents.Add(evento);
+                }
+                else
+                {
+                    Console.WriteLine("Non ci sono eventi in questa data");
+                }
+            }
+
+
+            return filterdEvents;
+        }
+        public static string PrintListEvent(List<Evento> listaEventi)
+        {
+            string result = "";
+
+            foreach (Evento evento in listaEventi)
+            {
+                result += $"{evento.getTitolo()}" + $"{evento.getData()}" + NewLine;
+            }
+
+            return result;
+        }
+        public string PrintEvent(List<Evento> listaEventi)
+        {
+            return PrintListEvent(listaEventi);
+        }
+
+        public List <Evento> GetEvent() 
+        {
+            return eventi;
+        }
+
+        public int NumberEvents()
+        {
+             return eventi.Count;
+        }
+        public void ClearEvents()
+        {
+            eventi.Clear();
+        }
     }
 }
